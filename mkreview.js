c85b707fc484ebef145271b8bf849aa92db1340e
Ewan <ewankjones@gmail.com>
2023-12-27 00:51:53 -0500

    json updates

--- mkreview.js
+++ mkreview.js
@@ -62,6 +62,299 @@ const data = {
           "Pretty good at showing heroin addiction but 90% of this movie is just 2 characters shooting up heroin and boozing"
         ],
         "cover": "https://m.media-amazon.com/images/M/MV5BYzRiODJiYjMtN2RiNy00NGVhLWJlN2MtODc4ZThiMGJmYzk0XkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg"
+      },
+      {
+        "when": 2023,
+        "mediaType": "M",
+        "name": "In the Mood for Love",
+        "score": "8/10",
+        "bullets": [
+          "The most unrealistic part of this movie is the guy makes money day trading",
+          "Made in 2000 but looks like it was made in the 70s ",
+          "So stylized on god",
+          "Legit the definition of show dont tell"
+        ],
+        "cover": "https://m.media-amazon.com/images/M/MV5BYWVjNjMwZTgtMGYyYy00NmVhLWE1NDItMzFhMmJkYTNjYWIwXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_.jpg"
+      },
+      {
+        "when": 2023,
+        "mediaType": "M",
+        "name": "Paranormal Activity",
+        "score": "1/10",
+        "bullets": [
+          "The most unrealistic part of this movie is the guy makes money day trading"
+        ],
+        "cover": "https://m.media-amazon.com/images/M/MV5BMjY1NjcxODQ4MV5BMl5BanBnXkFtZTcwMzUxNjM4Mg@@._V1_.jpg"
+      },
+      {
+        "when": 2023,
+        "mediaType": "M",
+        "name": "The Exorcist",
+        "score": "5/10",
+        "bullets": [
+          "The scariest part of this movie is its run time"
+        ],
+        "cover": "https://m.media-amazon.com/images/M/MV5BYWFlZGY2NDktY2ZjOS00ZWNkLTg0ZDAtZDY4MTM1ODU4ZjljXkEyXkFqcGdeQXVyMjUzOTY1NTc@._V1_.jpg"
+      },
+      {
+        "when": 2023,
+        "mediaType": "M",
+        "name": "The Northman",
+        "score": "6/10",
+        "bullets": [
+          "everyone looks like your average black metal band member",
+          "Inconsistent pacing",
+          "What’s with all this dog/wolf shit it’s kinda weird not going to lie",
+          "Didn’t mind the story but I feel like this kinda things been done better"
+        ],
+        "cover": "https://m.media-amazon.com/images/M/MV5BMzVlMmY2NTctODgwOC00NDMzLWEzMWYtM2RiYmIyNTNhMTI0XkEyXkFqcGdeQXVyNTAzNzgwNTg@._V1_.jpg"
+      },
+      {
+        "when": 2023,
+        "mediaType": "M",
+        "name": "The Lighthouse",
+        "score": "8/10",
+        "bullets": [
+          "My favourite lighthouse keeper robert Pattinson",
+          "Just gets better and better the further you get"
+        ],
+        "cover": "https://m.media-amazon.com/images/M/MV5BZmE0MGJhNmYtOWNjYi00Njc5LWE2YjEtMWMxZTVmODUwMmMxXkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg"
+      },
+      {
+        "when": 2023,
+        "mediaType": "M",
+        "name": "Unholy",
+        "score": "2/10",
+        "bullets": [
+          "Has some pretty bad acting",
+          "Most anticlimactic ending ice ever seen",
+          "Mary mary mary mary mary mary mary mary mary"
+        ],
+        "cover": "https://m.media-amazon.com/images/M/MV5BY2VhOTgxZGEtNGRiZi00NzVkLWE3MGEtZTRjNWEwY2MwZTI0XkEyXkFqcGdeQXVyMTkxNjUyNQ@@._V1_.jpg"
+      },
+      {
+        "when": 2023,
+        "mediaType": "M",
+        "name": "The VVitch: A New-England Folktale",
+        "score": "9/10",
+        "bullets": [
+          "Has old english dialog so parts are a little hard to understand",
+          "Not really a horror movie its more like watching a puriton family turn on each other",
+          "Whos the witch?",
+          "costumes , lighting, sets, and acting are all pretty much perfect for this"
+        ],
+        "cover": "https://m.media-amazon.com/images/M/MV5BMTUyNzkwMzAxOF5BMl5BanBnXkFtZTgwMzc1OTk1NjE@._V1_.jpg"
+      },
+      {
+        "when": 2023,
+        "mediaType": "T",
+        "name": "Alice in Borderland",
+        "sub": "Season 1 & 2",
+        "score": "7/10",
+        "bullets": [
+          "Imagine if you took an anime and made it live action",
+          "Face cards just kinda go “yah you right sorry boss” and then just die",
+          "Worlds decay is done well same with backstories",
+          "On god why is there a season 3 tease",
+          "Plot armour is crazy but the games are creative"
+        ],
+        "cover": "https://m.media-amazon.com/images/M/MV5BODI0OGIxM2MtZTQ0YS00ZjhhLWE3YzYtYzE2N2JkNThmY2RkXkEyXkFqcGdeQXVyODkzMzE0Nzc@._V1_.jpg"
+      },
+      {
+        "when": 2023,
+        "mediaType": "T",
+        "name": "Loki",
+        "sub": "Season 2",
+        "score": "4/10",
+        "bullets": [
+          "Uhhhhh whats going on here",
+          "A lot of science mambo jumbo that makes no sense",
+          "Typical marvel affair"
+        ],
+        "cover": "https://m.media-amazon.com/images/M/MV5BNTY1ZDQzNzQtZGM1Yy00YjRhLTliYmMtOGM2OWFlYTRjOTc2XkEyXkFqcGdeQXVyMTY3MDE5MDY1._V1_.jpg"
+      },
+      {
+        "when": 2023,
+        "mediaType": "M",
+        "name": "Black Swan",
+        "score": "8/10",
+        "bullets": [
+          "wat…",
+          "That fingernail scene is a little tough to watch"
+        ],
+        "cover": "https://m.media-amazon.com/images/M/MV5BNzY2NzI4OTE5MF5BMl5BanBnXkFtZTcwMjMyNDY4Mw@@._V1_.jpg"
+      },
+      {
+        "when": 2023,
+        "mediaType": "M",
+        "name": "Pearl",
+        "score": "7/10",
+        "bullets": [
+          "serial killer in the making the movie",
+          "Love how bright everything is (until it’s not)",
+          "Whoever played pearl did a great job",
+          "Would watch again just to pray on pearls downfall"
+        ],
+        "cover": "https://m.media-amazon.com/images/M/MV5BZTFkNmE5MjUtZDE1Yi00ZmQyLTk2YWUtN2EwODA1ZmNlOTA5XkEyXkFqcGdeQXVyMTM1MTE1NDMx._V1_.jpg"
+      },
+      {
+        "when": 2023,
+        "mediaType": "M",
+        "name": "Don: The Chase Begins Again",
+        "score": "6/10",
+        "bullets": [
+          "Song 3 out of 5 was the best one",
+          "It was don all along? But that doesn’t make much sense",
+          "Effects aren't the best  especially that skydiving green screen",
+          "Overall I’d say it was entertaining through and through to justify being 2:40 long",
+          "Creative sets and locations even if it’s all shot in 3 places"
+        ],
+        "cover": "https://m.media-amazon.com/images/M/MV5BMzAyMWE0MjgtMDVjNS00ZDMyLWE4NjQtNWU2ZDgyYTlmMjdjXkEyXkFqcGdeQXVyNjQ2MjQ5NzM@._V1_.jpg"
+      },
+      {
+        "when": 2023,
+        "mediaType": "G",
+        "name": "Starfield",
+        "sub": "[PC]",
+        "score": "6/10",
+        "bullets": [
+          "For a game about exploring there ain’t much of that",
+          "Bosses are pretty Garbo",
+          "Ships are cool",
+          "All the characters are pretty good",
+          "A lot and a lot of customization",
+          "Will probably be better with mods"
+        ],
+        "cover": "https://m.media-amazon.com/images/M/MV5BZGIxNDc4Y2EtNTgyZi00YWRjLWI1YzUtZDZmMDgzY2IwMDY4XkEyXkFqcGdeQXVyMTk2OTAzNTI@._V1_.jpg"
+      },
+      {
+        "when": 2023,
+        "mediaType": "T",
+        "name": "Ashoka",
+        "sub": "Season 1",
+        "score": "5/10",
+        "bullets": [
+          "droids are really well done",
+          "Kinda more of a rebels show which I’m not really a fan of",
+          "Poo ending way poo ending",
+          "All the characters are pretty good",
+          "Kinda want a show that just focuses on Ashoka",
+          "Redeemed by the one clone wars ep"
+        ],
+        "cover": "https://m.media-amazon.com/images/M/MV5BMWM1NjU1MDgtYjhjYS00Mjg5LWE1MWEtNzUwNWMxNTU2ZWY3XkEyXkFqcGdeQXVyMDM2NDM2MQ@@._V1_.jpg"
+      },
+      {
+        "when": 2023,
+        "mediaType": "G",
+        "name": "Lego Star Wars: The Skywalker Saga",
+        "sub": "[Switch]",
+        "score": "6/10",
+        "bullets": [
+          "didn’t run super well in handheld but worked pretty well in the dock",
+          "Story missions take a back stage to my need to collect everything and man is there a lot to collect",
+          "Started to lose interest in the sequel trilogy so much so that I took a year long break",
+          "First 6 episodes are great tho + give nostalgia for the complete saga for wii/ds"
+        ],
+        "cover": "https://m.media-amazon.com/images/M/MV5BOGI3Zjc3ZTItZjY4Yi00YTM4LWJiODctNzlkZTk2ZTVmMjI4XkEyXkFqcGdeQXVyMTk2OTAzNTI@._V1_.jpg"
+      },
+      {
+        "when": 2023,
+        "mediaType": "T",
+        "name": "Riverdale",
+        "sub": "Seasons 1-7",
+        "score": "3/10",
+        "bullets": [
+          "season 5 is some of the most jaw dropping bat shit insane tv I’ve ever seen",
+          "I’m 110% sure the whole crew behind this show is just as confused as I was watching it",
+          "There are at least 10 plots happening per episode",
+          "The last season is absolute dog compared to the rest",
+          "Some of the most delusional writing I’ve ever see in a show",
+          "Some of the dumbest one liners like \“highs and lows of high school football\”",
+          "Sucks to see my favorite 40 year  highscool students go but I can die happy now"
+        ],
+        "cover": "https://m.media-amazon.com/images/M/MV5BZjlmNWEyOTQtMzY4NC00ZGJiLWFmYzAtODhiZmYzZmY3Y2RlL2ltYWdlL2ltYWdlXkEyXkFqcGdeQXVyNjYzNDE4ODA@._V1_.jpg"
+      },
+      {
+        "when": 2023,
+        "mediaType": "T",
+        "name": "Heavenly Delusion",
+        "sub": "Season 1",
+        "score": "8/10",
+        "bullets": [
+          "Ain’t a lot of tropes in this one",
+          "Art is hella good",
+          "There are at least 10 plots happening per episode",
+          "The last season is absolute dog compared to the rest",
+          "All the characters and the world are pretty good",
+          "Just an adventure in the post apocalypse with the homies",
+        ],
+        "cover": "https://m.media-amazon.com/images/M/MV5BMjQ3NGE5NzItM2RlMi00YWE5LWJlOTAtYWRkMWRiY2Y0NmQ3XkEyXkFqcGdeQXVyMzgxODM4NjM@._V1_.jpg"
+      },
+      {
+        "when": 2023,
+        "mediaType": "T",
+        "name": "Bocchi the Rock!",
+        "sub": "Season 1",
+        "score": "5/10",
+        "bullets": [
+          "cutaways are pretty creative",
+          "There’s quite a lot of filler",
+          "All the characters work well together",
+          "Characters don’t really stray away from their archetypes",
+          "Extremely light hearted"
+        ],
+        "cover": "https://m.media-amazon.com/images/M/MV5BZTE3M2JkZjUtODQ4My00NzBlLTllZTctNDBlODcxNDU0MWNmXkEyXkFqcGdeQXVyNjc3NTI5MDY@._V1_.jpg"
+      },
+      {
+        "when": 2023,
+        "mediaType": "M",
+        "name": "The Blind Side",
+        "score": "2/10",
+        "bullets": [
+          "rich white woman saviour complex the movie"
+        ],
+        "cover": "https://m.media-amazon.com/images/M/MV5BMjEzOTE3ODM3OF5BMl5BanBnXkFtZTcwMzYyODI4Mg@@._V1_.jpg"
+      },
+      {
+        "when": 2023,
+        "mediaType": "M",
+        "name": "Edge of Tomorrow",
+        "score": "5/10",
+        "bullets": [
+          "Emily blunt is the best part of this movie",
+          "I want a spin off with just Emily blunt bro"
+        ],
+        "cover": "https://m.media-amazon.com/images/M/MV5BMTc5OTk4MTM3M15BMl5BanBnXkFtZTgwODcxNjg3MDE@._V1_.jpg"
+      },
+      {
+        "when": 2023,
+        "mediaType": "G",
+        "name": "Diablo 4",
+        "sub": "[PC]",
+        "score": "6/10",
+        "bullets": [
+          "the pre rendered cutscenes are nuts 10/10",
+          "The side content started to get really stale and uninteresting about half way through",
+          "Main story was dense but it’s like they took a page out of rockstars quest design",
+          "I ended up doing so much damage at the end that I was legit an unstoppable force",
+          "Good enemy variety and area design"
+        ],
+        "cover": "https://m.media-amazon.com/images/M/MV5BMTgyNDYyNGMtY2NkYi00OWEyLThkN2QtYzBjMzc3MmIyN2FjXkEyXkFqcGdeQXVyMTA0MTM5NjI2._V1_.jpg"
+      },
+      {
+        "when": 2023,
+        "mediaType": "T",
+        "name": "House of the Dragon",
+        "sub": "Season 1",
+        "score": "7/10",
+        "bullets": [
+          "all the names are the same and that’s confusing",
+          "Prefer the slower pace of the GOT",
+          "People out here playing 5d chess",
+          "Cg is good but has some definitely cheap looking shots",
+          "For a set up season there’s one spicy multi year political drama"
+        ],
+        "cover": "https://m.media-amazon.com/images/M/MV5BMjk2NjgzMTEtYWViZS00NTMyLWFjMzctODczYmQzNzk2NjIwXkEyXkFqcGdeQXVyMTEyMjM2NDc2._V1_.jpg"
       }
     ]
   }
